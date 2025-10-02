76e5494aa0c95cf9f8fdc574efed194c6615a781
Nikolai Shelekhov <nickshv13@icloud.com>
2025-10-02 15:04:59 +0700

    c

--- src/lib.rs
+++ src/lib.rs
@@ -56,6 +56,7 @@ pub mod pallet {
 		Created { owner: T::AccountId },
 		Transferred { from: T::AccountId, to: T::AccountId, kitty_id: [u8; 32] },
 		PriceSet { owner: T::AccountId, kitty_id: [u8; 32], new_price: Option<BalanceOf<T>> },
+		Sold { buyer: T::AccountId, kitty_id: [u8; 32], price: BalanceOf<T> },
 	}
 
 	#[pallet::error]
@@ -87,9 +88,23 @@ pub mod pallet {
 			Ok(())
 		}
 
-		pub fn set_price(origin: OriginFor<T>, kitty_id: [u8; 32], new_price: Option<BalanceOf<T>>) -> DispatchResult {
+		pub fn set_price(
+			origin: OriginFor<T>,
+			kitty_id: [u8; 32],
+			new_price: Option<BalanceOf<T>>,
+		) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 			Self::do_set_price(who, kitty_id, new_price)?;
+			Ok(())
+		}
+
+		pub fn buy_kitty(
+			origin: OriginFor<T>,
+			kitty_id: [u8; 32],
+			max_price: BalanceOf<T>,
+		) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+			Self::do_buy_kitty(who, kitty_id, max_price)?;
 			Ok(())
 		}
 	}
